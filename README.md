b284279f6038015ec3e9a704959f7b7488836075
Igor Kvachenok <darestade@gmail.com>
2023-12-07 00:27:27 +0100

    refactor code to work in local environment

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # generalSVR-generator
 
 [ruGPT3small](https://github.com/ai-forever/ru-gpts) model finetuned to make posts in the style of [GeneralSVR](https://t.me/s/generalsvr) telegram channel.  
-Repository contains:
-1. Python notebook which can be run from Google Colab.
-2. Raw and parsed data.
-3. A python script to parse data from telegram json.
+
+This repository contains:
+1. Python notebook run from local environment (requires CUDA).
+2. A python script to parse data from telegram json. Any posts data from any channel can be used.
+3. Raw and parsed data.
   
 ### Sample output:
 "Дорогие подписчики и гости канала! Владимир Путин съел яичницу на завтрак. Как сообщают очевидцы, президент не мог дождаться начала трапезы из-за сильной боли в голове или во время еды почувствовал запах жареного мяса у себя под носом (возможно он принимал душ по рекомендации врачей). Но это всего лишь предположения журналистов издания «Вести недели», которые говорят о том как много проблем с пищеварением возникает после принятия пищи со столь отвратительным перегаром.  В общем то вчера утром Путину стало плохо."
--- parser.py
+++ parser.py
@@ -6,43 +6,43 @@ import json
 import re
 
 
+DATAPATH = 'data/'
+
+
 def loader(filepath):
     with open(filepath) as file:
         return json.load(file)
 
 
-def parser(file, result=[]):
-    if result == []:
-        memory = file
-        result.append(' ')
-    for key, value in file.items():
-        if key == 'text' and value:
-            if not isinstance(value, list):
-                result.append(str(value)
-                              .replace('\xa0', ' ')
-                              .replace('\n', ' ')
-                              .strip())
-            elif isinstance(value[0], str):
-                result.append(str(value[0])
-                              .replace('\xa0', ' ')
-                              .replace('\n', ' ')
-                              .strip())
-        elif isinstance(value, dict):  # initiate tree search
-            parser(value, result)
-    try:  # execute this at the end of tree search
-        if file == memory:
-            reg = re.compile(r'^.*[Дд]орогие')  # remove ad posts from dataset
-            result = list(filter(reg.search, result))
-            return result
-    except UnboundLocalError:
-        pass
+def parser(file):
+    result = []
+    for element in file:  # navigating through list of dicts
+        for key, value in element.items():
+            if key == 'text' and value:  # getting posts content
+                if not isinstance(value, list):
+                    result.append(str(value)
+                                  .replace('\xa0', ' ')
+                                  .replace('\n', ' ')
+                                  .replace('  ', ' ')
+                                  .strip())
+                elif isinstance(value[0], str):
+                    result.append(str(value[0])
+                                  .replace('\xa0', ' ')
+                                  .replace('\n', ' ')
+                                  .replace('  ', ' ')
+                                  .strip())
+    # remove ad posts from dataset
+    # because all posts contain the same word
+    reg = re.compile(r'^.*[Дд]орогие')
+    result = list(filter(reg.search, result))
+    return result
 
 
-def main(filepath='./data.json'):
-    file = loader(filepath)
-    result_string = parser(file)
+def main():
+    file = loader(DATAPATH + 'data.json')
+    result_string = parser(file["messages"])
     result_string = json.dumps(result_string, ensure_ascii=False)
-    with open('parsed_data.json', 'w') as newfile:
+    with open(DATAPATH + 'parsed_data.json', 'w') as newfile:
         newfile.write(result_string)
 
 
